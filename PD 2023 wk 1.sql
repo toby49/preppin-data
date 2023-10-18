23654c32f9375e3eb8328ff9e2aae6b3a00e0840
toby49 <147813812+toby49@users.noreply.github.com>
2023-10-18 14:40:01 +0100

    Add files via upload

---
+++ PD 2023 wk 1.sql
@@ -0,0 +1,59 @@
+SELECT Split_part(transaction_code, '-', 1) AS Bank,
+
+//Renaming online or in-person values       
+       CASE
+         WHEN online_or_in_person = 1 THEN 'Online'
+         WHEN online_or_in_person = 2 THEN 'In-Person'
+       end                                  AS ONLINE_OR_IN_PERSON,
+
+//Renaming dates (dayofweek function returns a number)       
+       CASE Date_part(dayofweek, To_date(transaction_date,
+                                 'dd/mm/yyyy hh24:mi:ss'))
+         WHEN 0 THEN 'Monday'
+         WHEN 1 THEN 'Tuesday'
+         WHEN 2 THEN 'Wednesday'
+         WHEN 3 THEN 'Thursday'
+         WHEN 4 THEN 'Friday'
+         WHEN 5 THEN 'Saturday'
+         WHEN 6 THEN 'Sunday'
+       end                                  AS TRANSACTION_DATE
+
+FROM   pd2023_wk01
+LIMIT  100;
+
+
+//Output 1:
+SELECT Split_part(transaction_code, '-', 1) AS Bank,
+       Sum(value)                           AS Total_Value
+FROM   pd2023_wk01
+GROUP  BY bank;
+
+//Output 2:
+SELECT Split_part(transaction_code, '-', 1) AS Bank,
+       CASE
+         WHEN online_or_in_person = 1 THEN 'Online'
+         WHEN online_or_in_person = 2 THEN 'In-Person'
+       end                                  AS ONLINE_OR_IN_PERSON,
+       CASE Date_part(dayofweek, To_date(transaction_date,
+                                 'dd/mm/yyyy hh24:mi:ss'))
+         WHEN 0 THEN 'Monday'
+         WHEN 1 THEN 'Tuesday'
+         WHEN 2 THEN 'Wednesday'
+         WHEN 3 THEN 'Thursday'
+         WHEN 4 THEN 'Friday'
+         WHEN 5 THEN 'Saturday'
+         WHEN 6 THEN 'Sunday'
+       end                                  AS TRANSACTION_DAY,
+       Sum(value)                           AS Total_Value
+FROM   pd2023_wk01
+GROUP  BY Bank,
+          ONLINE_OR_IN_PERSON,
+          TRANSACTION_DAY;
+
+//Output 3:
+SELECT Split_part(transaction_code, '-', 1) AS Bank,
+       customer_code,
+       Sum(value)                           AS Total_Value
+FROM   pd2023_wk01
+GROUP  BY 1,
+          2; 
