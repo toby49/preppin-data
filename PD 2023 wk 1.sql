00a7354348eadf9eb2af693d411ebdba517bc940
toby49 <147813812+toby49@users.noreply.github.com>
2023-12-18 18:57:35 +0000

    Update PD 2023 wk 1.sql

--- PD 2023 wk 1.sql
+++ PD 2023 wk 1.sql
@@ -1,12 +1,12 @@
 SELECT Split_part(transaction_code, '-', 1) AS Bank,
 
-//Renaming online or in-person values       
+--Renaming online or in-person values       
        CASE
          WHEN online_or_in_person = 1 THEN 'Online'
          WHEN online_or_in_person = 2 THEN 'In-Person'
        end                                  AS ONLINE_OR_IN_PERSON,
 
-//Renaming dates (dayofweek function returns a number)       
+--Renaming dates (dayofweek function returns a number)       
        CASE Date_part(dayofweek, To_date(transaction_date,
                                  'dd/mm/yyyy hh24:mi:ss'))
          WHEN 0 THEN 'Monday'
@@ -22,13 +22,13 @@ FROM   pd2023_wk01
 LIMIT  100;
 
 
-//Output 1:
+--Output 1:
 SELECT Split_part(transaction_code, '-', 1) AS Bank,
        Sum(value)                           AS Total_Value
 FROM   pd2023_wk01
 GROUP  BY bank;
 
-//Output 2:
+--Output 2:
 SELECT Split_part(transaction_code, '-', 1) AS Bank,
        CASE
          WHEN online_or_in_person = 1 THEN 'Online'
@@ -50,7 +50,7 @@ GROUP  BY Bank,
           ONLINE_OR_IN_PERSON,
           TRANSACTION_DAY;
 
-//Output 3:
+--Output 3:
 SELECT Split_part(transaction_code, '-', 1) AS Bank,
        customer_code,
        Sum(value)                           AS Total_Value
